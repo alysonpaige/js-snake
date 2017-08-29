2b94ed3fdd598116ed8562af8c4125a4ccfafe63
Alyson Stiffel <alysonstiffel@gmail.com>
2017-08-29 13:30:56 -0400

    draw snake

--- app/assets/javascripts/snake.js
+++ app/assets/javascripts/snake.js
@@ -1,4 +1,9 @@
-var snake = [{ top: 0, left: 0}];
-var drawableSnake = { color: "green", pixels: snake };
-var drawableObjects = [drawableSnake];
-CHUNK.draw(drawableObjects);
+var drawSnake = function(snakeToDraw) {
+  var snake = [{ top: 0, left: 0}];
+  var drawableSnake = { color: "green", pixels: snake };
+  var drawableObjects = [drawableSnake];
+  CHUNK.draw(drawableObjects);
+};
+
+  var snake = [{ top: 0, left: 0}];
+  drawSnake(snake);
