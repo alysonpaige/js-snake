e140091eaa6c76a55e784eba9de6e46814e2b167
Alyson Stiffel <alysonstiffel@gmail.com>
2017-08-29 13:36:06 -0400

    move snake

--- app/assets/javascripts/snake.js
+++ app/assets/javascripts/snake.js
@@ -1,9 +1,15 @@
 var drawSnake = function(snakeToDraw) {
-  var snake = [{ top: 0, left: 0}];
   var drawableSnake = { color: "green", pixels: snake };
   var drawableObjects = [drawableSnake];
   CHUNK.draw(drawableObjects);
 };
 
-  var snake = [{ top: 0, left: 0}];
-  drawSnake(snake);
+var moveSnake = function(snake) {
+  var oldSegment = snake[0];
+  var newSegment = { top: oldSegment.top + 1, left: oldSegment.left };
+  var newSnake = [newSegment];
+  return newSnake;
+};
+
+var snake = [{ top: 0, left: 0}];
+drawSnake(snake);
